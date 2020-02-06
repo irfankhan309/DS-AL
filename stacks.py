ea519215258ef35d9ed5598bcc2a8217c9a14e52
irfankhan309 <irfanqehs@gmail.com>
2020-02-06 17:53:15 +0530

    intial commit

---
+++ stacks.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def get_stack(self):
+        return self.items
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+s = Stack()
+s.push(1)
+print(s.get_stack())
+print(s.peek())
+s.push('a')
+s.push('b')
+print(s.get_stack())
+print(s.peek())
