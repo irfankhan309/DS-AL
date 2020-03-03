755736b9620ec24734eb5c035f17aad2bd0270b5
irfankhan309 <irfanqehs@gmail.com>
2020-03-03 10:34:46 +0530

    csv

---
+++ CSV_File.py
@@ -0,0 +1,55 @@
+# comma seperated values
+# import csv
+# with open('emp.csv','w',newline='') as f:
+#     w = csv.writer(f)
+#     w.writerow(['ENO','ENAME','ESAL','EADDRESS'])
+#     n = int(input('enter how many employees data:'))
+#     for i in range(n):
+#         eno = int(input('enter employee number:'))
+#         ename = input('enter employee name:')
+#         esal = float(input('enter employee salary:'))
+#         eadd = input('enter employeee address:')
+#         w.writerow([eno,ename,esal,eadd])
+# print('totla employee data written to csv file successfully...')
+
+
+# reading csv file 
+# import csv
+# f = open('emp.csv','r') 
+# r = csv.reader(f)
+# data = list(r)
+# for line in data:
+#     for word in line:
+#         print(word,'\t\t',end='')
+#     print()
+
+
+# zipping and unzipping files
+# with zippiing memory utilization will be improved
+# transportation time will be reduced
+# import perfomance
+# we have zipfile module and classs name is ZipFile
+# f =ZipFile('files.zip','w','ZIP_DEFLATED')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
